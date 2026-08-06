0c9f0c77c668537edc4b1a3475c1b5ce75d61485
Roshini7788 <mandapalliroshini@gmail.com>
2026-08-06 14:10:50 +0530

    Create ResponsiveUI.dart

---
+++ ResponsiveUI.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const ResponsiveScreen(),
+    );
+  }
+}
+
+class ResponsiveScreen extends StatelessWidget {
+  const ResponsiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Responsive UI"),
+      ),
+      body: Center(
+        child: width < 600
+            ? const Text(
+                "Mobile Screen",
+                style: TextStyle(fontSize: 25),
+              )
+            : const Text(
+                "Large Screen",
+                style: TextStyle(fontSize: 25),
+              ),
+      ),
+    );
+  }
+}
